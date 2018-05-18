ea198265913ee408505e1e4415a10f29ee666287
MounikaReddy07 <38038620+MounikaReddy07@users.noreply.github.com>
2018-05-18 10:53:20 +0530

    first commit

---
+++ FirstVB/FirstVB/basic.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="vb" AutoEventWireup="false" CodeBehind="basic.aspx.vb" Inherits="FirstVB.basic" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    
+       
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+    </div>
+        <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+        <br />
+        <asp:Label ID="Label2" runat="server" Text="Label"></asp:Label>
+        <br />
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <br />
+        <asp:Button ID="Button1" runat="server" Text="check" />
+        <asp:Label ID="Label3" runat="server" Text="Label"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ FirstVB/FirstVB/basic.aspx.vb
@@ -0,0 +1,31 @@
+﻿Imports System
+
+Public Class basic
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+        Label1.Text = "First VB"
+
+        Dim Expression1 As Double
+        Expression1 = 5 * 3
+        Label2.Text = Expression1
+
+    End Sub
+    
+    Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If TextBox1.Text = IsNumeric() Then
+
+
+
+        End If
+
+        Dim i As Integer
+        If i > 0 Then
+            Label3.Text = "It is a positive number"
+
+            ' Response.Write("<script language=""javascript"">alert('It is a positive number');</script>")
+        End If
+
+    End Sub
+End Class
